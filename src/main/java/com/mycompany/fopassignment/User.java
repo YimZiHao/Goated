7bad201db3269e48a9a4fc9f946ac959d1e92f7d
YimZiHao <24210461@siswa.um.edu.my>
2025-11-11 23:11:09 +0800

    Wrote code for caesar cipher to encrypt password

--- src/main/java/com/mycompany/fopassignment/User.java
+++ src/main/java/com/mycompany/fopassignment/User.java
@@ -15,16 +15,31 @@ public class User {
     User(String emailAddress, String displayName, String password) {
         this.emailAddress = emailAddress;
         this.displayName = displayName;
-        this.password = password;
+        this.password = cipher(password);
     }
 
     @Override
     public String toString() {
         return "User{" + "emailAddress=" + emailAddress + ", password=" + password + ", displayName=" + displayName + '}';
     }
-
     
-
+    private String cipher(String password){
+        StringBuilder cipherText = new StringBuilder(); //using StringBuilder to append characters to ciphered password
+        char character;
+        for(int i = 0; i < password.length(); i++){
+            character = password.charAt(i);
+            if (character >= 32 && character <= 126){ 
+                //32 to 126 are the minimum to maximum printable ASCII characters (space to tilde ~)
+                character = (char) (((character - 32 + 55) % 95 + 95) % 95 + 32); 
+                //shift is 55, range is 95, %95 + 95 % 95 to make sure negative value is not returned
+                cipherText.append(character);
+            } else {
+                cipherText.append(character);
+            }
+        }
+        return cipherText.toString();
+    }
+    
     String getDisplayName() {
         return displayName;
     }
